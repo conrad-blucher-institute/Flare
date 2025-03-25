5cee3d864ff298666cd4a222702887f2fa107fbb
jeremiahsosa77 <jeremiahsosa27@gmail.com>
2025-03-25 14:25:50 -0500

    changed the deployment script for version 28 of docker

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,26 @@
 
 # A script that deploys flare and initializes its cron file.
 
-#Update to latest version of main
-git fetch origin --tags $1 
+# Lower active containers
+docker-compose down
+
+# Fetch origin            vvvvvv - This will delete any tags that origin has deleted
+git fetch origin --tags --prune --prune-tags
+
+# Checkout correct tag
+git checkout $1
 
 #Pull changes from tag
 git pull 
 
 #Remove first docker containers
-docker-compose down
+docker compose down
 
 #Build docker containers
-docker-compose build
+docker compose build
 
 #Launch docker containers
-docker-compose up -d
+docker compose up -d
 
 #Remove existing cron file
 crontab -r
