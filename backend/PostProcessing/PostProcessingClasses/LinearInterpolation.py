fe50d186a91f8950b1edf7416f25efdebc5ef164
CJQuintero61 <cjquintero61@gmail.com>
2025-08-04 14:35:28 -0500

    changed to consectutive NaNs

--- backend/PostProcessing/PostProcessingClasses/LinearInterpolation.py
+++ backend/PostProcessing/PostProcessingClasses/LinearInterpolation.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-#ImmediateArithmeticOperation.py
+# LinearInterpolation.py
 #-------------------------------
-# Created By : Matthew Kastl
+# Created By: Christian Quintero
+# Last Updated: 08/04/2025
 #-------------------------------
-""" The post processing in this file preforms an linear interpolation of a column.
+""" The post processing in this file performs a linear interpolation of a column.
  """ 
 #-------------------------------
 # 
@@ -24,7 +25,7 @@ class LinearInterpolation(IPostProcessing):
             df DataFrame: The DataFrame containing this collation of the data that has been collected.
             col_name: str - The column in the DataFrame to interpolate.
             interpolation_interval: int - The interval to interpolate data on, in seconds.
-            limit: int - The amount of Nan's in a row it will interpolate.
+            limit: int - The amount of Nan's consecutive NaNs in a row to interpolate.
 
         Returns:
             DataFrame : A new dataframe obj might have to be created, this will always be a reference to the most updated version.
@@ -41,7 +42,7 @@ class LinearInterpolation(IPostProcessing):
             }
         },
         """
-        
+
         # Isolate the data we are going to interpolate
         data = df[col_name]
 
@@ -49,22 +50,29 @@ class LinearInterpolation(IPostProcessing):
         # the data to the specific interval we want to interpolate on.
         data = data.reindex(date_range(start=data.index[0], end=data.index[-1], freq=timedelta(seconds=interpolation_interval)))
 
-        # We want to not interpolate if there are too many Nans in a row. However the pandas limit parameter only stops interpolation once its
-        # counted a cumulative sum of Nans higher than limit. Thus it keeps the Nans in that group where the cumulative sum was still < limit.
-        # The forwards cumulative mask looks for where this mistake will happen thus used to overwrite the interpolated values with Nan.
+        # We want to 'mask' the data so that each value gets assigned TRUE if NaN and FALSE if it is a real value.
         nan_mask = data.isna()
-        cumulative_nan_streaks = nan_mask.groupby(~nan_mask).cumsum()
-        forward_cumulative_nan_mask = cumulative_nan_streaks.gt(limit)
 
-        # We do the interpolation backwards because a forwards cumulative mask was easier to write than a backwards one.
-        backwards_interpolation = data.interpolate(method= 'time', limit= limit, limit_area= 'inside', limit_direction= 'backward')
-        
-        # Here we repair the mistake by looking at the forward cumulative mask and inserting Nan.
-        backwards_interpolation[forward_cumulative_nan_mask] = np.nan
-   
-        # Outer join to preserve all data in the dataframe.
+        # Create unique group IDs for consecutive NaN streaks.
+        group_ids = (~nan_mask).cumsum()
+
+        # Count the length of each NaN streak
+        # For each group ID, count how many NaN values (True values in nan_mask) are in that group
+        streak_lengths = nan_mask.groupby(group_ids).transform('sum')   
+
+        # Create a mask for NaN streaks longer than the limit
+        # We only want to mark actual NaN positions that are in long streaks
+        long_streak_mask = (streak_lengths > limit) & nan_mask
+
+        # Interpolate all NaN values using time-based interpolation, which is the same as linear interpolation
+        interpolated_data = data.interpolate(method='time', limit_area='inside')
+
+        # Put NaNs back where streaks were too long
+        # aka where the NaN streaks > limit
+        interpolated_data[long_streak_mask] = np.nan
+
+        # Replace the original column with our processed data
         df.drop(columns=[col_name], inplace=True)
-        df = df.join(backwards_interpolation, how='outer')
+        df = df.join(interpolated_data, how='outer')
         return df
-
 
