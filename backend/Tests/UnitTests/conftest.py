fd940c72b6b189cb0f99fb8e80f80f52e5a83f7b
Florence Tissot <ftissot@fastmail.us>
2025-08-28 17:01:07 -0500

    cleaned up code in pytest config file

--- backend/Tests/UnitTests/conftest.py
+++ backend/Tests/UnitTests/conftest.py
@@ -1,6 +1,9 @@
 import sys
 from pathlib import Path
 
-# Add backend folder to path
-backend_dir = Path(__file__).parent.parent.parent
+# Add the 'backend' folder to the sys path
+# since this file is in Flare/backend/Tests/UnitTests/, 
+# we need to go two folders up to get the path to the backend folder
+backend_dir = Path(__file__).resolve().parents[2]
+#we insert that path on the front so the python interpreter finds it faster (yeah, yeah, a little tacky ..)
 sys.path.insert(0, str(backend_dir))
