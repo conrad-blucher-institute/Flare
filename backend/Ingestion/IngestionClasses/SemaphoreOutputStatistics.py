84936754dc58eedba56d6fe46bdbd906f39ec405
Christian Quintero <cjquintero61@gmail.com>
2026-05-14 18:16:01 -0500

    updated to match api changes

--- backend/Ingestion/IngestionClasses/SemaphoreOutputStatistics.py
+++ backend/Ingestion/IngestionClasses/SemaphoreOutputStatistics.py
@@ -24,7 +24,7 @@ class SemaphoreOutputStatistics(IDataIngestion):
         Ingests data from a Semaphore API endpoint
 
         :param data: dataframe - the dataframe object to fill with data
-        :param ref_time: datetime - the reference time for the data request
+        :param ref_time: datetime - the reference time for the data request, this is unused for this ingestion class
         :param model_names: list[str] - the names of the models to request data for
 
         :returns: dataframe - a new dataframe with the ingested data added
@@ -32,11 +32,12 @@ class SemaphoreOutputStatistics(IDataIngestion):
         url = self.__prepare_url(model_names)
 
         response = api_request(url)
-        is_valid_response = self.__validate_response(response, model_names)
-        if not is_valid_response:
+        validated_response = self.__validate_response(response)
+
+        if validated_response is None:
             return add_empty_column(data, "Water Temperature Prediction Statistics")
 
-        return self.__add_data(df= data, response=response, model_names=model_names)
+        return self.__add_data(df= data, response=validated_response)
 
 
     def __prepare_url(self, model_names: list[str]) -> str:
@@ -59,17 +60,69 @@ class SemaphoreOutputStatistics(IDataIngestion):
         return url[:-1]  # remove the trailing '&'
     
 
-    def __validate_response(self, response: list[dict], model_names: list[str]) -> bool:
+    def __validate_response(self, response: dict) -> dict[str, dict] | None:
         '''
         This function validates the response from the semaphore API by checking for
         empty responses and missing data
 
-        :params response: list[dict] - the response from the API request
+        :params response: dict - the response from the API request
             the response will look like 
-            [
-                {
+            {
+                "CRPS_6hr": {
                     "modelName": "CRPS_6hr",
-                    "timeGenerated": "2026-05-12T12:00:00+00:00",
+                    "timeGenerated": "2026-05-12T12:00:00",
+                    "p1": 25.20813787460327,
+                    "p5": 25.744343280792236,
+                    "p10": 25.94855365753174,
+                    "p25": 26.22447681427002,
+                    "p50": 26.485905647277832,
+                    "p75": 26.73847484588623,
+                    "p90": 27.00280132293701,
+                    "p95": 27.201376342773436,
+                    "p99": 27.95702182769775,
+                    "min": 22.700056076049805,
+                    "max": 29.92579460144043,
+                    "mean": 26.486501573524475,
+                    "std_dev": 0.48822468208073955
+                },
+                "model_that_doesn't_exit": null,
+                ...
+            }
+
+        :returns dict[str, dict] - a dictionary containing only valid data that has statistics or None if no valid data is present
+        '''
+        logger = thread_storage.logger
+        validated_response = {}
+
+        if response is None: 
+            return None
+        
+        # filter out nulls in the response to only include models that compute statistics
+        for key, value in response.items():
+            if value is None:
+                logger.log_info(f'Warning:: Model {key} missing in returned data!')
+                continue
+            else:
+                validated_response[key] = value
+
+        # catch cases where all models are missing or have no data
+        if validated_response == {}:
+            return None
+        else:
+            return validated_response
+    
+    
+    def __add_data(self, df: DataFrame, response: dict[str, dict]) -> DataFrame:
+        '''
+        Takes the validated response dict and adds it into the dataframe
+
+        :param df: dataframe - the dataframe to add the data to
+        :param response: dict[str, dict] - the validated response from the API request
+            the response will look like 
+            {
+                "CRPS_6hr": {
+                    "modelName": "CRPS_6hr",
+                    "timeGenerated": "2026-05-12T12:00:00",
                     "p1": 25.20813787460327,
                     "p5": 25.744343280792236,
                     "p10": 25.94855365753174,
@@ -85,84 +138,32 @@ class SemaphoreOutputStatistics(IDataIngestion):
                     "std_dev": 0.48822468208073955
                 },
                 ...
-            ]
-        :params model_names: list[str] - the names of the models we queried for
-
-        :returns: bool
-            - true if we got a valid response, even if there is missing data
-            - false if the response was empty
-        '''
-        logger = thread_storage.logger
-
-        if response is None: 
-            return False
-        
-        for dict in response:
-            model_name = dict.get("modelName")
-            if model_name is None: 
-                logger.log_info(f'Warning:: Model {model_name} missing in returned data!')
-                continue
-
-        return True
-    
-    
-    def __add_data(self, df: DataFrame, response: list[dict], model_names: list[str]) -> DataFrame:
-        '''
-        Takes the data returned by the semaphore API and parses it into the dataframe
-
-        :param df: dataframe - the dataframe to add the data to
-        :param response: list[dict] - the response from the API request
-            the response will look like 
-                [
-                    {
-                        "modelName": "CRPS_6hr",
-                        "timeGenerated": "2026-05-12T12:00:00+00:00",
-                        "p1": 25.20813787460327,
-                        "p5": 25.744343280792236,
-                        "p10": 25.94855365753174,
-                        "p25": 26.22447681427002,
-                        "p50": 26.485905647277832,
-                        "p75": 26.73847484588623,
-                        "p90": 27.00280132293701,
-                        "p95": 27.201376342773436,
-                        "p99": 27.95702182769775,
-                        "min": 22.700056076049805,
-                        "max": 29.92579460144043,
-                        "mean": 26.486501573524475,
-                        "std_dev": 0.48822468208073955
-                    },
-                    ...
-                ]
-        :param model_names: list[str] - the names of the models we queried for
+            }
 
         :returns: dataframe - the dataframe with the new data added
         '''
         logger = thread_storage.logger
 
         rows = []
-        for dict in response:
-            model_name  = dict['modelName']
-            ############################
-            # DONT FORGET TO REMOVE THE TZ INFO AFTER JOY UPDATES THE API
-            ############################
-            timeGenerated = datetime.strptime(dict['timeGenerated'], '%Y-%m-%dT%H:%M:%S%z')
-            timeGenerated = timeGenerated.replace(tzinfo=None) # remove timezone info for easier handling
-            
+        for _, value in response.items():
             '''
             since the lead time isn't explicitly given in the response, we have to
-            calculate it since the lead times are tied to the model name
+            extract it since the lead times are tied to the model name
             
             Ex) CRPS_6hr has a lead time of 6 hours, so the 6 is extracted and 6 hours are added
                 to the time generated to get the verified time which is used as the index for the data
             '''
+            model_name = value['modelName']
             lead_time = int(re.search(r'(\d+)hr', model_name).group(1))
+            timeGenerated = datetime.strptime(value['timeGenerated'], '%Y-%m-%dT%H:%M:%S')
             verifiedTime = timeGenerated + timedelta(hours=lead_time)
+
             row = {'verifiedTime': verifiedTime}
             for stat in self.STATISTICS:
-                row[f'Water Temperature Prediction {stat}'] = dict.get(stat, np.nan)
+                row[f'Water Temperature Prediction {stat}'] = value.get(stat, np.nan)
             rows.append(row)
 
         df_stats = DataFrame(rows).set_index('verifiedTime')
 
-        # Add this to the collation df with an outerjoin to ensure all data is preserved
+        # join the data frames with an outer join to ensure all data is preserved
         return df.join(df_stats, how='outer')
